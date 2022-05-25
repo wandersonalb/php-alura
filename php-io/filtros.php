7967ebab755bc6648f633902473a66386747a3d7
Wanderson Alves Barbosa <wanderson.barbosa@meta.com.br>
2022-05-25 18:38:15 -0300

    Criação de filtro para streams

---
+++ php-io/filtros.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'MeuFiltro.php';
+
+$arquivoCursos = fopen('lista-cursos.txt', 'r');
+
+stream_filter_register('alura.partes', MeuFiltro::class);
+
+stream_filter_append($arquivoCursos, 'alura.partes');
+
+echo fread($arquivoCursos, filesize('lista-cursos.txt'));
